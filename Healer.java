8caef481ab72c3ff27a41939919c11339acb3215
ardianagoes <ardianagoes83@gmail.com>
2025-01-10 09:15:42 -0600

    added attack

--- Healer.java
+++ Healer.java
@@ -8,10 +8,34 @@ public class Healer extends Adventurer {
   }
 
   public Healer(String name){
-    this(name, Math.random() * 3 + 24);
+    this(name, (int) Math.random() * 3 + 24);
   }
 
   public Healer(){
     this("Bob");
+  }
+
+  public getSpecialname(){
+    return "elixir";
+  }
+
+  public getSpecial(){
+    return elixir;
+  }
+
+  public getSpecialmax(){
+    return elixirMax;
+  }
+
+  public void setSpecial(int n){
+    elixir = n;
+  }
+
+  public String attack(Adventurer other, Adventurer ally1, Adventurer ally2){
+    int damage = (int)Math.random() + 1;
+    other.applyDamage(damage);
+    int heal
+    other.setHP(other.getHP() + (2 * damage));
+    return this + "uses Divine Strike on" + other " and dealt " + damage " points of damage, healing their allies"
   }
 }
