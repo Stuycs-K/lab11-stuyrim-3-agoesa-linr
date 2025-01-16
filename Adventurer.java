76bbfd681dec00af9f2f782c3c429c92640476f2
Angelo Chen <achen65@stuy.edu>
2025-01-16 09:59:27 -0500

    more merge

--- Adventurer.java
+++ Adventurer.java
@@ -65,7 +65,7 @@ public abstract class Adventurer{
       this.HP -= amount * 0.25;
     }
     else{
-      this.HP -= amount;  
+      this.HP -= amount;
   }
   }
 
@@ -153,8 +153,7 @@ public abstract class Adventurer{
     this.stun = n;
   }
 
-<<<<<<< HEAD
-=======
+
   public int getProtect(){
     return this.protect;
   }
@@ -162,7 +161,7 @@ public abstract class Adventurer{
     this.protect = n;
   }
 
->>>>>>> 0610e4eddf7ce985f767433ec63a631af1913462
+
   public void debuff(){
     // actually do the stuff of the debuffs. The prior commands are to tell you the info about it.This is where we actually do it.
   }
