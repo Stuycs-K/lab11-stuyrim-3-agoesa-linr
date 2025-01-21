6e8f1a35e383a2b60451dd7083a744cc92798351
Ricky Lin <rlin61@stuy.edu>
2025-01-21 01:44:06 -0500

    Added ifstun to every move

--- Chimera.java
+++ Chimera.java
@@ -47,6 +47,9 @@ public class Chimera extends Adventurer{
 
 //"Rampage" Deals 6-10 dmg to one enemy, poisoning them for 2 turns, which reduces enemy attack damage by 75% and deals 1 damage per turn. Chimera gains 1 Rage.
   public String attack(Adventurer other){
+    if (ifStunned()){
+      return "" + this.getName() + " is stunned! Their turn is skipped.";
+    }
     int damage = (int)(Math.random()* 5) + 6; // 0 to 1 -> 0 to 5 -> 6 to 11 exclusive.
     other.applyDamage(damage);
     this.restoreSpecial(1);
@@ -56,6 +59,9 @@ public class Chimera extends Adventurer{
   }
 //"Chimera's wrath" Chimera does an AOE stomp, dealing 8 dmg to all enemies, while breathing fire, lighting all of its enemies on fire for 5 turns. 15 rage required.
   public String specialAttack(Adventurer other){
+    if (ifStunned()){
+      return "" + this.getName() + " is stunned! Their turn is skipped.";
+    }
     if (getSpecial() >= 15){
       for (int i = 0; i < getEnemies().size(); i++){
         getEnemies().get(i).applyDamage(8);
@@ -72,11 +78,17 @@ public class Chimera extends Adventurer{
 
 
   public String support(Adventurer other){
+    if (ifStunned()){
+      return "" + this.getName() + " is stunned! Their turn is skipped.";
+    }
     return support();
 
   }
   //"Limitless adaptation"Chimera heals itself for 25% of its current HP and nullifies all of its bad status effects. Gains 1 rage.
   public String support(){
+    if (ifStunned()){
+      return "" + this.getName() + " is stunned! Their turn is skipped.";
+    }
     int health = this.getHP() / 4;
     this.setHP(health);
     this.restoreSpecial(1);
